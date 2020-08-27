ffdc047fc47d437164facf922156112a81d587d1
Yunusa Onivehu <obadobay@gmail.com>
2020-08-27 22:14:34 +0100

    styling

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-
-
 function insert(num) {
-    document.form.textview.value = document.form.textview.value + num;
+  document.form.textview.value = document.form.textview.value + num;
 }
 function equal() {
-        let exp = document.form.textview.value;
-    if (exp) {
-        document.form.textview.value = eval(exp);
-    }
+  let exp = document.form.textview.value;
+  if (exp) {
+    document.form.textview.value = eval(exp);
+  }
 }
 
 function clean() {
-    let cl = document.form.textview.value;
-    if (cl) {
-        document.form.textview.value = "";
-    }
+  let cl = document.form.textview.value;
+  if (cl) {
+    document.form.textview.value = "";
+  }
 }
 function pie() {
-    let pe = document.form.textview.value;
-    if (pe) {
-        document.form.textview.value = pe * 3.142;
-    }
+  let pe = document.form.textview.value;
+  if (pe) {
+    document.form.textview.value = pe * 3.142;
+  }
 }
 
 function back() {
-    let bck = document.form.textview.value;
-    if (bck) {
-        document.form.textview.value = bck.substring(0, bck.length-1);
-    }
+  let bck = document.form.textview.value;
+  if (bck) {
+    document.form.textview.value = bck.substring(0, bck.length - 1);
+  }
 }
